f25d95c1c901fdadf14b3986ca9c76c44a83fe07
Simon Kotchou <54844121+Simon-Kotchou@users.noreply.github.com>
2022-01-16 02:41:38 -0700

    Add files via upload

---
+++ README.md
@@ -0,0 +1,5 @@
+# Klein2
+Field Session Summer 2020
+
+run setup.ps1 in windows powershell to install all dependincies for the python script process_data.py
+ 
---
+++ setup.ps1
@@ -0,0 +1,7 @@
+pip install pandas
+pip install matplotlib
+pip install networkx
+pip install datetime
+pip install numpy
+pip install sklearn
+pip install joblib
